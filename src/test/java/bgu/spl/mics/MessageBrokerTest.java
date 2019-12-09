b823f5c247407540d4a2a283526169c0aabf5505
Zisling <43644953+Zisling@users.noreply.github.com>
2019-12-09 22:54:31 +0200

    this if f**ked up

--- src/test/java/bgu/spl/mics/MessageBrokerTest.java
+++ src/test/java/bgu/spl/mics/MessageBrokerTest.java
@@ -2,97 +2,138 @@ package bgu.spl.mics;
 
 import bgu.spl.mics.example.messages.ExampleBroadcast;
 import bgu.spl.mics.example.messages.ExampleEvent;
+import bgu.spl.mics.example.publishers.ExampleMessageSender;
 import bgu.spl.mics.example.subscribers.ExampleBroadcastSubscriber;
 import bgu.spl.mics.example.subscribers.ExampleEventHandlerSubscriber;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class MessageBrokerTest {
     MessageBroker toTest;
-    Subscriber testSub1;
-    Subscriber testSub2;
+    Subscriber[] subArr = new Subscriber[4];
+    boolean[] subIsRegister = new boolean[4];
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         toTest = MessageBrokerImpl.getInstance();
-        testSub1 =new ExampleBroadcastSubscriber("Agent 47", new String[]{"1"});
-        testSub2= new ExampleEventHandlerSubscriber("diana", new String[] {"2"});
+        Subscriber testSub1 = new ExampleBroadcastSubscriber("Agent 47", new String[]{"1"});
+        Subscriber testSub2 = new ExampleEventHandlerSubscriber("John Bradford", new String[]{"1"});
+        Subscriber testSub3 = new ExampleMessageSender("Diana", new String[]{"broadcast"});
+        Subscriber testSub4 = new ExampleMessageSender("Revolver Ocelot", new String[]{"event"});
+        subArr[0] = testSub1;
+        subArr[1] = testSub2;
+        subArr[2] = testSub3;
+        subArr[3] = testSub4;
     }
 
     @AfterEach
-    public void tearDown(){
-        toTest=null;
-        testSub1=null;
+    public void tearDown() {
+        for (int i = 0; i < subIsRegister.length; i++) {
+            if (subIsRegister[i]) {
+                toTest.unregister(subArr[i]);
+            }
+        }
+        Arrays.fill(subArr, null);
+        toTest = null;
+
     }
 
     @Test
-    public void test(){
+    public void test() {
+
         //TODO: change this test and add more tests :)
-        fail("Not a good test");
+//        fail("Not a good test");
     }
 
     @Test
     void getInstance() {
         try {
             assertNotNull(MessageBrokerImpl.getInstance());
+        } catch (Exception e) {
+            fail("Unexpeted Exception " + e.getMessage());
         }
-        catch (Exception e){fail("Unexpeted Exception "+e.getMessage());}
     }
 
     @Test
-    void register_BroadcastSubscriber() {
+    void register_unregister_BroadcastSubscriber() {
         try {
-            toTest.register(testSub1);
-        }catch (Exception e){fail("unexcited EXCEPTION "+e.getMessage());}
-    }
-    @Test
-    void register_EventHandlerSubscriber() {
+            toTest.register(subArr[0]);
+        } catch (Exception e) {
+            fail("didnt register a BroadcastSubscriber unexcited EXCEPTION " + e.getMessage());
+        }
         try {
-            toTest.register(testSub2);
-        }catch (Exception e){fail("unexcited EXCEPTION "+e.getMessage());}
+            toTest.unregister(subArr[0]);
+        } catch (Exception e) {
+            fail("didnt unregister a BroadcastSubscriber unexcited EXCEPTION " + e.getMessage());
+        }
     }
 
     @Test
-    void subscribeEvent() {
+    void register_unregister_EventHandlerSubscriber() {
         try {
-            toTest.subscribeEvent(ExampleEvent.class,testSub2);
-        }catch (Exception e){fail("unexcited EXCEPTION "+e.getMessage());}
-    }
-
-    @Test
-    void subscribeBroadcast() {
+            toTest.register(subArr[1]);
+        } catch (Exception e) {
+            fail("didnt register a EventHandlerSubscriber unexcited EXCEPTION " + e.getMessage());
+        }
         try {
-            toTest.subscribeBroadcast(ExampleBroadcast.class,testSub2);
-        }catch (Exception e){fail("unexcited EXCEPTION "+e.getMessage());}
+            toTest.unregister(subArr[1]);
+        } catch (Exception e) {
+            fail("didnt unregister a EventHandlerSubscriber unexcited EXCEPTION " + e.getMessage());
+        }
     }
 
     @Test
-    void sendBroadcast() {
+    void register_unregister_MessageSender() {
         try {
-            toTest.sendBroadcast(new ExampleBroadcast("Agent 47"));
-        }catch (Exception e){fail("unexcited EXCEPTION "+e.getMessage());}
-    }
-
-    @Test
-    void sendEvent() {
+            toTest.register(subArr[2]);
+        } catch (Exception e) {
+            fail("didnt register a MessageSender unexcited EXCEPTION " + e.getMessage());
+        }
         try {
-        toTest.sendEvent(new ExampleEvent("Agent 47"));
-        } catch (Exception e){fail("unexcited EXCEPTION "+e.getMessage());}
+            toTest.unregister(subArr[2]);
+        } catch (Exception e) {
+            fail("didnt unregister a MessageSender unexcited EXCEPTION " + e.getMessage());
+        }
+    }
+
+
+    @Test
+    void testSubscribeEvent() {
+        Thread sub = new Thread(subArr[1]);
+        sub.start();
+        try {
+            synchronized (this) {
+                this.wait(100);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ExampleEvent send = new ExampleEvent("X-com");
+        toTest.subscribeEvent(ExampleEvent.class, subArr[1]);
+        assertNotNull(toTest.sendEvent(send));
+        sub.interrupt();
     }
 
     @Test
-    void complete(){
-
-    }
-
-    @Test
-    void unregister() {
-    }
-
-    @Test
-    void awaitMessage() {
+    void testSubscribeBrodcast() {
+        Thread sub = new Thread(subArr[0]);
+        sub.start();
+        try {
+            synchronized (this) {
+                this.wait(100);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ExampleBroadcast send = new ExampleBroadcast("Hitman");
+        toTest.subscribeBroadcast(ExampleBroadcast.class, subArr[0]);
+        toTest.sendBroadcast(send);
+        sub.interrupt();
     }
 }
