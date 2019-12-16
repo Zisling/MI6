8d59bcc9426a44995309b62ca04ac65e4bd01749
Zisling <43644953+Zisling@users.noreply.github.com>
2019-12-16 17:17:24 +0200

    some staff

--- src/main/java/bgu/spl/mics/MessageBrokerImpl.java
+++ src/main/java/bgu/spl/mics/MessageBrokerImpl.java
@@ -1,17 +1,36 @@
 package bgu.spl.mics;
 
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 /**
  * The {@link MessageBrokerImpl class is the implementation of the MessageBroker interface.
  * Write your implementation here!
  * Only private fields and methods can be added to this class.
  */
 public class MessageBrokerImpl implements MessageBroker {
-	static MessageBroker instance;
+	private static MessageBroker instance;
+	private ConcurrentHashMap<Class<? extends Event<?>>, ConcurrentLinkedQueue<Subscriber>> eventMap;
+	private ConcurrentHashMap<Class<? extends Broadcast>, ConcurrentLinkedQueue<Subscriber>> broadcastMap;
+	private ConcurrentHashMap<Subscriber,ConcurrentLinkedQueue<Message>> subMap;
+
+
+//	constructor
+	private MessageBrokerImpl(){
+		eventMap = new ConcurrentHashMap<>();
+		broadcastMap = new ConcurrentHashMap<>();
+		subMap = new ConcurrentHashMap<>();
+	}
+
 	/**
 	 * Retrieves the single instance of this class.
 	 */
 	public static MessageBroker getInstance() {
-		if (instance==null){instance=new MessageBrokerImpl();}
+		if(instance==null){
+			synchronized (MessageBrokerImpl.class){
+		if (instance==null){instance=new MessageBrokerImpl();}}}
 		return instance;
 	}
 
@@ -48,14 +67,12 @@ public class MessageBrokerImpl implements MessageBroker {
 
 	@Override
 	public void register(Subscriber m) {
-		// TODO Auto-generated method stub
-
+		subMap.put(m, new ConcurrentLinkedQueue<>());
 	}
 
 	@Override
 	public void unregister(Subscriber m) {
-		// TODO Auto-generated method stub
-
+		subMap.remove(m);
 	}
 
 	@Override
