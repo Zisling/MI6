13125e013cf6f5bbf9bd4f1a60b85f2cf506ff2a
speednir123 <50735091+speednir123@users.noreply.github.com>
2019-12-27 14:27:10 +0200

    Cleaning Generalizing MessageBroker

--- src/main/java/bgu/spl/mics/application/Broadcasts/AbortBroadCast.java
+++ src/main/java/bgu/spl/mics/application/Broadcasts/AbortBroadCast.java
@@ -1,12 +1,11 @@
 package bgu.spl.mics.application.Broadcasts;
 
-import bgu.spl.mics.Broadcast;
 import bgu.spl.mics.Event;
 
 import java.util.List;
 
 public class AbortBroadCast implements Event<Boolean> {
-    List<String > agentToRelease;
+    private List<String > agentToRelease;
 
     public AbortBroadCast(List<String> agentToRelease) {
         this.agentToRelease = agentToRelease;
--- src/main/java/bgu/spl/mics/application/Broadcasts/TickBroadcast.java
+++ src/main/java/bgu/spl/mics/application/Broadcasts/TickBroadcast.java
@@ -5,18 +5,30 @@ import bgu.spl.mics.Broadcast;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TickBroadcast implements Broadcast{
-    private AtomicInteger TimeTick;
+    private AtomicInteger TimeTicker;
     private int Tick;
 
+    /**
+     * TickBroadcast's Constructor
+     * @param timeTick-Sets TimeTicker of TickBroadcast to Provided timeTick value.
+     */
     public TickBroadcast(AtomicInteger timeTick) {
-        TimeTick = timeTick;
+        TimeTicker = timeTick;
         Tick = timeTick.get();
     }
 
+    /**
+     *Returns TimerTicker Reference.
+     * @return TimeTick-returns the TimeTick
+     */
     public AtomicInteger getTimeTick() {
-        return TimeTick;
+        return TimeTicker;
     }
 
+    /**
+     * Returns the current Tick value of TickBroadcast.
+     * @return tick-returns the tick of TickBroadcast.
+     */
     public int getTick() {
         return Tick;
     }
