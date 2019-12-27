5979473996485292eefcc4dc1cd2889f963e4857
speednir123 <50735091+speednir123@users.noreply.github.com>
2019-12-27 14:04:23 +0200

    Cleaning Generalizing MessageBroker

--- src/main/java/bgu/spl/mics/application/Events/GadgetAvailableEvent.java
+++ src/main/java/bgu/spl/mics/application/Events/GadgetAvailableEvent.java
@@ -2,6 +2,9 @@ package bgu.spl.mics.application.Events;
 
 import bgu.spl.mics.Event;
 
+/**
+ * Gadget
+ */
 public class GadgetAvailableEvent implements Event<Integer> {
     private String gadget;
     public GadgetAvailableEvent(String gadget){
