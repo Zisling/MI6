4b569707dccc5ed1a810d659f46c674bcecc4586
speednir123 <50735091+speednir123@users.noreply.github.com>
2019-12-27 14:03:31 +0200

    Cleaning Generalizing MessageBroker

--- src/main/java/bgu/spl/mics/application/Events/AgentAvailableEvent.java
+++ src/main/java/bgu/spl/mics/application/Events/AgentAvailableEvent.java
@@ -4,12 +4,24 @@ import bgu.spl.mics.Event;
 
 import java.util.List;
 
+/**
+ * Event that checks if the Agents specified are available for a mission,Sent by M to MoneyPenny through Message Broker
+ */
 public class AgentAvailableEvent implements Event<Integer> {
     private List<String> serialAgentsNumbers;
+
+    /**
+     * Constructor of AgentAvailableEvent
+     * @param serialAgentsNumbers-sets the field to the serialAgentsNumbers provided.
+     */
     public AgentAvailableEvent(List<String> serialAgentsNumbers){
         this.serialAgentsNumbers= serialAgentsNumbers;
     }
 
+    /**
+     * Returns the serial numbers of agents required for the mission.
+     * @return serialAgentsNumbers-
+     */
     public List<String> getSerialAgentsNumbers() {
         return serialAgentsNumbers;
     }
--- src/main/java/bgu/spl/mics/application/Events/ReadyEvent.java
+++ src/main/java/bgu/spl/mics/application/Events/ReadyEvent.java
@@ -4,17 +4,33 @@ import bgu.spl.mics.Event;
 
 import java.util.List;
 
+
+/**
+ * ReadyEvent-Send agents to the mission
+ */
+
 public class ReadyEvent implements Event<List<String>> {
     private  int duration;
     private List<String> serialAgentsNumbers;
+
+    //ReadyEvent's Constructor
     public ReadyEvent(int duration, List<String> serialAgentsNumbers) {
         this.duration = duration;
         this.serialAgentsNumbers= serialAgentsNumbers;
     }
 
+    /**
+     * Returns the serial numbers of agents required for the mission.
+     * @return serialAgentsNumbers- returns the serial numbers of agents.
+     */
     public List<String> getSerialAgentsNumbers() {
         return serialAgentsNumbers;
     }
+
+    /**
+     * returns the duration of the mission
+     * @return duration-Duration of the mission
+     */
 
     public int getDuration() {
         return duration;
